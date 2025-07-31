df88c62e391ebfaeb1abf919474432ab7c1a6d1b
Sibelle-Roziski <sibelle.roziski@escola.pr.gov.br>
2025-07-31 10:45:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+const perguntas = [
+    {
+    categoria: "[Categoria do Aluno]",
+    pergunta: "[qual a capital do Brasil]",
+    resposta: "[Brasilia]"
+    },
+    {
+    categoria: "[Categoria do Aluno]",
+    pergunta: "[Quem pintou a Mona Lisa]",
+    resposta: "[Leonardo da Vinci]"
+    },
+    {
+    categoria: "[Categoria do Aluno]",
+    pergunta: "[Qual o maior planeta do sistema solar]",
+    resposta: "[Jupiter]"
+    },
+    {
+    categoria: "[Categoria do Aluno]",
+    pergunta: "[Em qual ano o Brasil proclamou a republica]",
+    resposta: "[1992]"
+    },
+    {
+    categoria: "[Categoria do Aluno]",
+    pergunta: "[Qual a forma quimica da agua]",
+    resposta: "[H2O]"
+    }
+    ];
